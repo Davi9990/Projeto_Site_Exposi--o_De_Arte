6725607d11c3349b5adbd1db341058335195e56a
Davi9990 <162383055+Davi9990@users.noreply.github.com>
2024-12-26 18:56:04 -0300

    Site_Exposicao

---
+++ Model/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>De Passagem</title>
+        <link rel="stylesheet" href="../Visual/Visual.css">
+    </head>
+    <body>
+            <div class ="circle"><img src= https://s10.aconvert.com/convert/p3r68-cdx67/anrhi-tev66.png></div>
+    
+            <h2 class="Tamanho2">Bem Vindo ao Site </h2>
+            <h1 class="Tamanho">De Passagem</h1>
+            <p class="letras">A Exposição foi pensada e idealizada pelos seguintes alunos do curso
+            de tecnologia em jogos digitais:
+            </p>
+
+            <div class ="Scrollar">
+                <div class ="Caixa1">
+                    <div class="Caixa2">
+
+                    </div>
+                </div>
+            </div>
+    </body>
+</html>
