593b3135263612aaee4758f8b93e464a1ebbc93c
Davi9990 <162383055+Davi9990@users.noreply.github.com>
2024-12-29 11:57:30 -0300

    Projeto_Site_Exposicao

--- Model/index.php
+++ Model/index.php
@@ -6,7 +6,7 @@
         <link rel="stylesheet" href="../Visual/Visual.css">
     </head>
     <body>
-            <div class ="circle"><img src= https://s10.aconvert.com/convert/p3r68-cdx67/anrhi-tev66.png></div>
+            <div class ="circle"><img src= https://s10.aconvert.com/convert/p3r68-cdx67/av1uu-65j1w.png></div>
     
             <h2 class="Tamanho2">Bem Vindo ao Site </h2>
             <h1 class="Tamanho">De Passagem</h1>
@@ -17,9 +17,61 @@
             <div class ="Scrollar">
                 <div class ="Caixa1">
                     <div class="Caixa2">
-
+                        <div class="retrato"><img src="https://s10.aconvert.com/convert/p3r68-cdx67/axakl-psf9t.png"></div>
                     </div>
+                    <p class="letras2">Davi Ferreira Lima Caridade</p>
                 </div>
+
+                <div class ="Caixa1">
+                    <div class="Caixa2">
+                        <div class="retrato"><img src="https://s10.aconvert.com/convert/p3r68-cdx67/agyrd-sbcg7.png"></div>
+                    </div>
+                    <p class="letras2">Andrey Pereira Rozendo</p>
+                </div>
+
+                <div class ="Caixa1">
+                    <div class="Caixa2">
+                        <div class="retrato"><img src="https://s10.aconvert.com/convert/p3r68-cdx67/apncc-3pe8n.png"></div>
+                    </div>
+                    <p class="letras2">Cainã Guimarães Soarez</p>
+                </div>
+
+                <div class ="Caixa1">
+                    <div class="Caixa2">
+                        <div class="retrato"><img src="https://s10.aconvert.com/convert/p3r68-cdx67/ag3ah-da259.png"></div>
+                    </div>
+                    <p class="letras2">Wendel Duarte de Oliveira</p>
+                </div>
+
+                <div class ="Caixa1">
+                    <div class="Caixa2">
+                        <div class="retrato"><img src="https://s10.aconvert.com/convert/p3r68-cdx67/ad74e-ieocy.png"></div>
+                    </div>
+                    <p class="letras2">Lusk Raulzito da Silva</p>
+                </div>
+
+                <div class ="Caixa1">
+                    <div class="Caixa2">
+                        <div class="retrato"><img src="https://s10.aconvert.com/convert/p3r68-cdx67/axc8g-v6rus.png"></div>
+                    </div>
+                    <p class="letras2">Daniel Fernandes Argolo</p>
+                </div>
+
+                <div class ="Caixa1">
+                    <div class="Caixa2">
+                        <div class="retrato"><img src="https://s10.aconvert.com/convert/p3r68-cdx67/a8hf2-sr9vz.png"></div>
+                    </div>
+                    <p class="letras2">João Vitor Jacinto da Silva</p>
+                </div>
+            </div>
+
+            <p class="letras">Seguindo as orientações da Professora de Edição de imagens 2: </p>
+
+            <div class="Caixa1_Professora">
+                <div class="Caixa2_Professora">
+                    <div class="retrato2"><img src="https://s10.aconvert.com/convert/p3r68-cdx67/ayqoc-zzffh.png"></div>
+                </div>
+                <p class="letras3">Jessica Menezes</p>
             </div>
     </body>
 </html>
