767b527e96fb3c42478abf13d64d070dd71a5a61
Davi9990 <162383055+Davi9990@users.noreply.github.com>
2025-01-02 18:07:46 -0300

    Site_Imagens

--- Model/index.php
+++ Model/index.php
@@ -6,7 +6,7 @@
         <link rel="stylesheet" href="../Visual/Visual.css">
     </head>
     <body>
-            <div class ="circle"><img src= https://s10.aconvert.com/convert/p3r68-cdx67/av1uu-65j1w.png></div>
+            <div class ="circle"><img src= "../Imagens/Logo_Para_Redes_Sociais.png"></div>
     
             <h2 class="Tamanho2">Bem Vindo ao Site </h2>
             <h1 class="Tamanho">De Passagem</h1>
@@ -17,49 +17,49 @@
             <div class ="Scrollar">
                 <div class ="Caixa1">
                     <div class="Caixa2">
-                        <div class="retrato"><img src="https://s10.aconvert.com/convert/p3r68-cdx67/axakl-psf9t.png"></div>
+                        <div class="retrato"><img src="../Imagens/Davi_Ferreira_Lima_Caridade2.png"></div>
                     </div>
                     <p class="letras2">Davi Ferreira Lima Caridade</p>
                 </div>
 
                 <div class ="Caixa1">
                     <div class="Caixa2">
-                        <div class="retrato"><img src="https://s10.aconvert.com/convert/p3r68-cdx67/agyrd-sbcg7.png"></div>
+                        <div class="retrato"><img src="../Imagens/Andrey_Roseno.png"></div>
                     </div>
                     <p class="letras2">Andrey Pereira Rozendo</p>
                 </div>
 
                 <div class ="Caixa1">
                     <div class="Caixa2">
-                        <div class="retrato"><img src="https://s10.aconvert.com/convert/p3r68-cdx67/apncc-3pe8n.png"></div>
+                        <div class="retrato"><img src="../Imagens/Cainã3.png"></div>
                     </div>
                     <p class="letras2">Cainã Guimarães Soarez</p>
                 </div>
 
                 <div class ="Caixa1">
                     <div class="Caixa2">
-                        <div class="retrato"><img src="https://s10.aconvert.com/convert/p3r68-cdx67/ag3ah-da259.png"></div>
+                        <div class="retrato"><img src="../Imagens/Wendel3.png"></div>
                     </div>
                     <p class="letras2">Wendel Duarte de Oliveira</p>
                 </div>
 
                 <div class ="Caixa1">
                     <div class="Caixa2">
-                        <div class="retrato"><img src="https://s10.aconvert.com/convert/p3r68-cdx67/ad74e-ieocy.png"></div>
+                        <div class="retrato"><img src="../Imagens/Lucas3.png"></div>
                     </div>
                     <p class="letras2">Lusk Raulzito da Silva</p>
                 </div>
 
                 <div class ="Caixa1">
                     <div class="Caixa2">
-                        <div class="retrato"><img src="https://s10.aconvert.com/convert/p3r68-cdx67/axc8g-v6rus.png"></div>
+                        <div class="retrato"><img src="../Imagens/Daniel2.png"></div>
                     </div>
                     <p class="letras2">Daniel Fernandes Argolo</p>
                 </div>
 
                 <div class ="Caixa1">
                     <div class="Caixa2">
-                        <div class="retrato"><img src="https://s10.aconvert.com/convert/p3r68-cdx67/a8hf2-sr9vz.png"></div>
+                        <div class="retrato"><img src="../Imagens/João2.png"></div>
                     </div>
                     <p class="letras2">João Vitor Jacinto da Silva</p>
                 </div>
@@ -69,9 +69,27 @@
 
             <div class="Caixa1_Professora">
                 <div class="Caixa2_Professora">
-                    <div class="retrato2"><img src="https://s10.aconvert.com/convert/p3r68-cdx67/ayqoc-zzffh.png"></div>
+                    <div class="retrato2"><img src="../Imagens/Jessica_Fernandes2.png"></div>
                 </div>
                 <p class="letras3">Jessica Menezes</p>
+            </div>
+
+            <p class="letras"> Essa exposição tem como principal objetivo realizar críticas é reflexões sobre a sociedade brasileira atual, por meio das imagens. </p>
+
+            <p class="letras"> Para visualizar as artes propostas acesse o painel logo abaixo para ver qual categoria deseja se adentrar: </p>
+
+            <div class="Painel">
+                <div class="Paineizinhos">
+                    <p class="letras4"> 1.Desigualdade Social</p>
+                </div>
+                
+                <div class="Paineizinhos">
+                    <p class="letras4"> 2.Ancestralidade</p>
+                </div>
+
+                <div class="Paineizinhos">
+                    <p class="letras4"> 3.Afrofuturismo</p>
+                </div>
             </div>
     </body>
 </html>
